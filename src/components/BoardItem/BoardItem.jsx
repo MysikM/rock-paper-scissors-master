b313c815225dbc8a8c455661a022cf66c7df291f
Mysik <Mishamysik@gmail.com>
2022-05-12 11:55:43 +0300

    main board create

--- src/components/BoardItem/BoardItem.jsx
+++ src/components/BoardItem/BoardItem.jsx
@@ -1,12 +1,17 @@
 import React, {useContext} from 'react';
 import {LvlContext} from "../../context/Toggle";
+import './board-item.scss';
 
 const BoardItem = ({src, title}) => {
     const {toggle} = useContext(LvlContext);
 
     return (
-        <div className={`board ${title} ${toggle && 'lvlUp'}`}>
-            {title}
+        <div className={`item ${title} ${toggle && 'lvlUp'}`}>
+            <button className='item--container'>
+                <span className='item--wrapper'>
+                    <img src={src} alt={title}/>
+                </span>
+            </button>
         </div>
     );
 };
