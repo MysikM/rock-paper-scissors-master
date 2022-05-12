a3e9d952259d8e04ff415f98ad4b91887b5797e4
Mysik <Mishamysik@gmail.com>
2022-05-12 20:15:57 +0300

    match visual add

--- src/components/MatchBoard/MatchBoard.jsx
+++ src/components/MatchBoard/MatchBoard.jsx
@@ -8,7 +8,16 @@ const MatchBoard = () => {
     return (
         <section className='match'>
             <div className='match--player'>
-                <BoardItem key={boardItems[0].title} src={boardItems[0].src} title={boardItems[0].title} choose={true}/>
+                <h5>You picked</h5>
+                <BoardItem src={boardItems[0].src} title={boardItems[0].title} choose={true}/>
+            </div>
+            <div className='match--menu'>
+                <h2>You lose</h2>
+                <button>Play again</button>
+            </div>
+            <div className='match--computer'>
+                <h5>The house picked</h5>
+                <BoardItem src={boardItems[1].src} title={boardItems[1].title} choose={true}/>
             </div>
         </section>
     );
