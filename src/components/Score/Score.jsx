dd3a5f81600b920e7b0f8ded0af6cc623d76f50e
Mysik <Mishamysik@gmail.com>
2022-05-12 10:38:47 +0300

    header section create

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ScoreContext} from "../../context/Score";
+import './score.scss';
 
 const Score = () => {
+    const {score} = useContext(ScoreContext);
     return (
-        <div>
-            Score
+        <div className='score'>
+            <span className='score--title'>score</span>
+            <span className='score--amount'>{score}</span>
         </div>
     );
 };
